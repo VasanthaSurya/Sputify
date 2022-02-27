ba08c25f69fcc1867174d6621ea945d803264445
VasanthaSurya <57515752+VasanthaSurya@users.noreply.github.com>
2022-02-27 13:12:39 +0530

    added index.js file

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import reactDOM from 'react-dom';
+
+const App = () =>{
+    return (
+        <div className='container'>
+            <div className='border w-25 border-round'>
+                <h1>
+                    Sputify
+                </h1>
+                <p>Lets Replicate Spotify.</p>
+            </div>
+        </div>
+    )
+}

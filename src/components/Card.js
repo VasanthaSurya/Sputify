77f07643c686a085f3f6dcf3a35b8391b4a1933d
VasanthaSurya <57515752+VasanthaSurya@users.noreply.github.com>
2022-04-15 19:18:49 +0530

    ..

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,31 +6,34 @@ class Card extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
-            bgcolor : 'bg-dark',
-            display : 'd-none'
+            bgcolor : 'dark-secondary',
+            display : 'd-none',
+            transform :'tbefore'
         }
         this.before = this.before.bind(this);
         this.after = this.after.bind(this);
     }
     before() {
         this.setState({
-            bgcolor : 'bg-dark',
-            display : 'd-none'
+            bgcolor : 'dark-secondary',
+            display : 'd-none',
+            transform :'tbefore'
         })
     }
     after() {
         this.setState({
-            bgcolor : 'bg-secondary',
-            display : ''
+            bgcolor : 'bg-dark',
+            display : '',
+            transform :'tafter'
         })
     }
     render() {
         return (
             <div 
-            className={"card d-flex col " + this.state.bgcolor}
+            className={"card d-flex col " + this.state.bgcolor +" "+ this.state.transform}
             onMouseEnter={this.after}
             onMouseLeave={this.before}
-            style={{width:"12rem",height:"18rem"}}>
+            style={{width:"12rem",height:"16rem"}}>
                 <img className="card-img-top p-2 w-100 h-60 position-relative" src={logoimg} alt="Card" />
                 <a className="position-absolute top-50 end-0" href="">
                     <img src={play} 
@@ -43,7 +46,7 @@ class Card extends React.Component{
                 </a>
                 <div className="card-body overflow-hidden">
                     <h6 className="font-weight-bold card-title text-white user-select-none">{this.props.cardTitle}</h6>
-                    <p className="card-text text-white user-select-none">Some quick example text to build on the card.</p>
+                    <p className="card-text text-white user-select-none small-text">Some quick example text to build on the card.</p>
                 </div>
             </div>
         )
